b7f30dad4fbbb3a6f05859d0bc225c9992650479
Valerkaa <mr.valetnikov.74@gmail.com>
2023-12-26 15:26:59 +0200

    Start projectX

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Routes,Route} from "react-router";
+import {MainPage} from "./pages/MainPage/MainPage";
+import {WorkerPage} from "./pages/WorkerPage/WorkerPage";
+import {GiverPage} from "./pages/GiverPage/GiverPage";
+import {ContactPage} from "./pages/ContactPage/ContactPage";
+import "./styles/_global.scss"
+import {Navigation} from "./components/GlobalComponents/Header/Navigation";
+function App() {
+  document.body.classList.add('black');
+  return (
+    <>
+      <Navigation/>
+    <Routes>
+      <Route path="/" element={<MainPage/>} />
+      <Route path="/worker" element={<WorkerPage/>} />
+      <Route path="/employer" element={<GiverPage/>} />
+      <Route path="/contact" element={<ContactPage/>} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
