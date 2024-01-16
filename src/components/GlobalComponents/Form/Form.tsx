b1c65ddad7098bcbd4fc8f3753b3f8b218c951dd
Valerkaa <mr.valetnikov.74@gmail.com>
2024-01-16 12:39:40 +0200

    Rework Valid

--- src/components/GlobalComponents/Form/Form.tsx
+++ src/components/GlobalComponents/Form/Form.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import Particles, {initParticlesEngine} from "@tsparticles/react";
 import {
     type Container,
@@ -13,8 +13,8 @@ import PhoneInput from 'react-phone-input-2'
 import {validEmail, validNaming, validPhone, validSelect} from "./Validation";
 
 
-
 export function Form(this: any) {
+    // eslint-disable-next-line
     const [init, setInit] = useState(false);
 
     useEffect(() => {
@@ -31,10 +31,10 @@ export function Form(this: any) {
     // @ts-ignore
     const options: ISourceOptions = useMemo(
         () => ({
-            style:{
-              width:100,
-              height:100,
-              position:"absolute",
+            style: {
+                width: 100,
+                height: 100,
+                position: "absolute",
             },
             fpsLimit: 120,
             particles: {
@@ -74,17 +74,22 @@ export function Form(this: any) {
 
     type FormDataValue = {
         fullName: string,
-        vacancy : string,
+        vacancy: string,
         email: string,
         phone: string,
-        employment:string,
-        city:string,
+        employment: string,
+        city: string,
+        experience: string,
+        englvl: string,
     }
+    // eslint-disable-next-line
     const {register, handleSubmit, formState: {errors}} = useForm<FormDataValue>();
     const onSubmit = (data: any) => {
         console.log(data);
     };
-    // console.log(errors)
+    const onError = () => {
+        console.log(errors)
+    }
     return (
         <section className={styles.formWrap}>
             <Particles
@@ -102,33 +107,64 @@ export function Form(this: any) {
                        className={styles.lastnameValid} placeholder="Last name" onChange={validNaming}/>
                 <input type="text" className="email-valid" placeholder="Email" {...register("email", {
                     required: true,
+                    // eslint-disable-next-line
                     pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                 })} onChange={validEmail}/>
                 <select  {...register("employment", {required: true})} onChange={validSelect}>
                     <option value="">Вид зайнятості</option>
-                    <option value="Mrs">Mrs</option>
-                    <option value="Miss">Miss</option>
-                    <option value="Dr">Dr</option>
+                    <option value="Повна">Повна</option>
+                    <option value="Неповна">Неповна</option>
+                    <option value="Відалено">Відалено</option>
                 </select>
-                <select {...register("city", {required: true})}>
-                    <option value="">Mr</option>
-                    <option >Mrs</option>
-                    <option >Miss</option>
-                    <option >Dr</option>
+                <select {...register("city", {required: true})} onChange={validSelect}>
+                    <option value="">Місто</option>
+                    <option value="Усі">Усі</option>
+                    <option value="Київ">Київ</option>
+                    <option value="Вінниця">Вінниця</option>
+                    <option value="Дніпро">Дніпро</option>
+                    <option value="Івано-Франківськ">Івано-Франківськ</option>
+                    <option value="Житомир">Житомир</option>
+                    <option value="Запоріжжя">Запоріжжя</option>
+                    <option value="Львів">Львів</option>
+                    <option value="Миколаїв">Миколаїв</option>
+                    <option value="Одеса">Одеса</option>
+                    <option value="Тернопіль">Тернопіль</option>
+                    <option value="Харків">Харків</option>
+                    <option value="Хмельницький">Хмельницький</option>
+                    <option value="Черкаси">Черкаси</option>
+                    <option value="Чернігів">Чернігів</option>
+                    <option value="Чернівці">Чернівці</option>
+                    <option value="Ужгород">Ужгород</option>
+                </select>
+                <select  {...register("englvl", {required: true})} onChange={validSelect}>
+                    <option value="">Рівень англійської:</option>
+                    <option value="Елементарний">Елементарний</option>
+                    <option value="Нижчий за середній">Нижчий за середній</option>
+                    <option value="Середній">Середній</option>
+                    <option value="Рівень вище середнього">Рівень вище середнього</option>
+                    <option value="Просунутий">Просунутий</option>
+                    <option value="Вільний рівень">Вільний рівень</option>
+                </select>
+                <select  {...register("experience", {required: true})} onChange={validSelect}>
+                    <option value="">Досвід роботи:</option>
+                    <option value="Будь-який досвід роботи">Будь-який досвід роботи</option>
+                    <option value="Менше 1 року">Менше 1 року</option>
+                    <option value="1-2 роки">1-2 роки</option>
+                    <option value="2-3 роки">2-3 роки</option>
+                    <option value="3-5 років">3-5 років</option>
+                    <option value="Понад 5 років">Понад 5 років</option>
                 </select>
                 <PhoneInput
                     onChange={validPhone}
                     inputProps={{
                         name: 'phone',
                         required: true,
-
                     }}
                     onlyCountries={['ua']}
                     placeholder="Введіть номер телефона"
                     country={'ua'}
-
                 />
-                <button type="submit" className={styles.sendBtn}>Submit a request</button>
+                <button type="submit" onClick={onError} className={styles.sendBtn}>Submit a request</button>
             </form>
         </section>
     )
--- src/components/GlobalComponents/Form/Validation.tsx
+++ src/components/GlobalComponents/Form/Validation.tsx
@@ -1,54 +1,50 @@
 import {ChangeEvent} from "react";
 import styles from "./form.module.scss";
 
+// @ts-ignore
+const setValidClass = (event) => {
+    event.target.classList.add(`${styles.activeBad}`)
+    event.target.classList.remove(`${styles.activeGood}`)
+    console.log(event.target.value.length)
+}
+const setInvalidClass = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    event.target.classList.add(`${styles.activeBad}`)
+    event.target.classList.remove(`${styles.activeGood}`)
+}
+
 export function validNaming(event: ChangeEvent<HTMLInputElement>) {
-    if (event.target.value.length > 2){
-        event.preventDefault()
-        event.target.classList.add(`${styles.activeGood}`)
-        event.target.classList.remove(`${styles.activeBad}`)
-        
-    }else{
-        event.preventDefault()
-        
-        event.target.classList.add(`${styles.activeBad}`)
-        event.target.classList.remove(`${styles.activeGood}`)
+    if (event.target.value.length > 2) {
+        setValidClass(event)
+        return;
     }
+    setInvalidClass(event)
 }
-export function validEmail(event: ChangeEvent<HTMLInputElement>){
+
+export function validEmail(event: ChangeEvent<HTMLInputElement>) {
+    // eslint-disable-next-line
     let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    validEmail = pattern.test(String(event.target.value).toLowerCase());
-    if (validEmail){
-        event.preventDefault()
-        event.target.classList.add(`${styles.activeGood}`)
-        event.target.classList.remove(`${styles.activeBad}`)
-        
-    }else{
-        event.preventDefault()
-        
-        event.target.classList.add(`${styles.activeBad}`)
-        event.target.classList.remove(`${styles.activeGood}`)
+        validEmail = pattern.test(String(event.target.value).toLowerCase());
+    if (validEmail) {
+        setValidClass(event)
+        return;
     }
+    setInvalidClass(event)
 }
-export function validPhone(value:string,data:{},event: ChangeEvent<HTMLInputElement>){
+
+export function validPhone(value: string, data: {}, event: ChangeEvent<HTMLInputElement>) {
     let pattern = /^\+?3?8?(0\d{9})$/;
-    if (pattern.test(value)){
-        event.target.classList.add(`${styles.activeGood}`)
-        event.target.classList.remove(`${styles.activeBad}`)
-        
-    }else{
-        
-        event.target.classList.add(`${styles.activeBad}`)
-        event.target.classList.remove(`${styles.activeGood}`)
+    if (pattern.test(value)) {
+        setValidClass(event)
+        return;
     }
+    setInvalidClass(event)
+    return;
 }
-export function validSelect(event: ChangeEvent<HTMLSelectElement>){
-    if (event.target.value){
-        event.target.classList.add(`${styles.activeGood}`)
-        event.target.classList.remove(`${styles.activeBad}`)
 
-    }else{
-
-        event.target.classList.add(`${styles.activeBad}`)
-        event.target.classList.remove(`${styles.activeGood}`)
+export function validSelect(event: ChangeEvent<HTMLSelectElement>) {
+    if (event.target.value) {
+        setValidClass(event)
+        return;
     }
+    setInvalidClass(event)
 }
