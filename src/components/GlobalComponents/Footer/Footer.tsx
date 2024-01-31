e6e3930f3e58060f861df74029391399238376db
Valerkaa <mr.valetnikov.74@gmail.com>
2024-01-31 16:54:01 +0200

    Added contact info

--- src/components/GlobalComponents/Footer/Footer.tsx
+++ src/components/GlobalComponents/Footer/Footer.tsx
@@ -27,8 +27,8 @@ export function Footer() {
                     <div className={styles.about}>
                         <p>Ви можете зв'язатися з нами в будь-який час, наша команда завжди на зв'язку!</p>
                         <div className="">
-                            <a href="tel:+32235253235">Номер телефону</a>
-                            <a href="mailto:nashapo4ta@gmail.com">Пошта</a>
+                            <a href="tel:+380 66 392 86 44">+380 66 392 86 44</a>
+                            <a href="mailto:info@head-hunters-league.com">info@head-hunters-league.com</a>
                             <ul className={styles.social}>
                                 <a href=""><li><FontAwesomeIcon icon={faInstagram} /></li></a>
                                 <a href=""><li><FontAwesomeIcon icon={faLinkedinIn}/></li></a>
